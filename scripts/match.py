45e2e6a9ccad563869ddd839f41b59d168d3f9b9
Evgeny Arbatov <arbatovevgeny@gmail.com>
2025-05-16 22:33:21 +0700

    Simplify matching script

--- scripts/match.py
+++ scripts/match.py
@@ -5,7 +5,7 @@ import json
 import pandas as pd
 
 OSRM_URL = "http://localhost:6000/match/v1/foot/"
-MATCH_RADIUS_METERS = 10
+MATCH_RADIUS_METERS = 20
 
 def get_nodes(lat, lon):
     response = requests.get(f"http://127.0.0.1:6000/nearest/v1/foot/{lon},{lat}")
@@ -16,18 +16,17 @@ def get_nodes(lat, lon):
         return []
 
     waypoints = data['waypoints']
-    return [node for wp in waypoints for node in wp['nodes']]
+    return [int(node) for wp in waypoints for node in wp['nodes']]
       
 def get_ways(nodes):
     node_1, node_2 = nodes
     overpass_query = f"""
         [out:json];
         node(id:{node_1},{node_2});
-        way(bn)["highway"~"^(pedestrian|footway|residential|path)$"]
-        ["crossing"!~"traffic_signals"]
-        ["footway"!~"crossing"];
+        way(bn);
         out ids;
     """
+
     response = requests.get(
         "http://localhost:8000/api/interpreter", 
         params={'data': overpass_query}
@@ -53,7 +52,7 @@ def get_matched_pair(coord1, coord2):
                 
                 nodes = get_nodes(lat, lon)
                 ways = get_ways(nodes)
-                
+        
                 matched_coords.append([lat, lon, ways])
         return matched_coords
     except requests.RequestException as e:
