c423dfef77cb447058aae1cc9660a433f1d3d80b
Evgeny Arbatov <arbatovevgeny@gmail.com>
2025-12-18 11:33:31 +0700

    chore: format python code with black

--- scripts/filter.py
+++ scripts/filter.py
@@ -7,27 +7,29 @@ from geopy.distance import geodesic
 
 FILTER_DISTANCE_METERS = 100
 
+
 def filter_by_distance(df):
     filtered = [df.iloc[0]]
-    
+
     for idx in range(1, len(df)):
-        current_point = (df.iloc[idx]['lat'], df.iloc[idx]['lon'])
+        current_point = (df.iloc[idx]["lat"], df.iloc[idx]["lon"])
         too_close = False
-        
+
         for kept in filtered:
-            kept_point = (kept['lat'], kept['lon'])
+            kept_point = (kept["lat"], kept["lon"])
             if geodesic(current_point, kept_point).meters <= FILTER_DISTANCE_METERS:
                 too_close = True
                 break
-        
+
         if not too_close:
             filtered.append(df.iloc[idx])
-    
+
     filtered_df = pd.DataFrame(filtered)
     return filtered_df
 
+
 def main(
-    gpx_csv_file, 
+    gpx_csv_file,
     filtered_gpx_csv_file,
 ):
     df = pd.read_csv(gpx_csv_file)
@@ -35,6 +37,7 @@ def main(
     df = filter_by_distance(df)
 
     df.to_csv(filtered_gpx_csv_file, index=False)
+
 
 if __name__ == "__main__":
     main(*sys.argv[1:])
